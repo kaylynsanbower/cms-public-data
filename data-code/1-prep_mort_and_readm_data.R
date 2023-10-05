33602a4699706414b22b73acb2e9e57a525e8300
kaylynsanbower <kaylyn.sanbower@gmail.com>
2023-10-05 16:36:42 -0400

    add output name.

--- data-code/1-prep_mort_and_readm_data.R
+++ data-code/1-prep_mort_and_readm_data.R
@@ -5,3 +5,6 @@
 df <- read.csv("data/input/Complications_and_Deaths-Hospital.csv", header=T)
 
 
+# make this the final file to match the output that already exists.
+
+write.csv(export, file = "HC_MORT_READM.csv")
